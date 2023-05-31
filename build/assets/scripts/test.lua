898a0e5d104161fff31076ed5866ceb448ebb554
M4LC0LM123 <78646308+M4LC0LM123@users.noreply.github.com>
2023-05-31 22:50:44 +0200

    mac lua

--- build/assets/scripts/test.lua
+++ build/assets/scripts/test.lua
@@ -5,21 +5,20 @@ currSpeed = scaleSpeed
 maxScale = 100
 
 function update(entity)
-
     entity.scale.x = entity.scale.x + currSpeed * GetFrameTime()
     entity.scale.y = entity.scale.y + currSpeed * GetFrameTime()
 
     if entity.scale.x >= maxScale then
         currSpeed = -scaleSpeed
     end
-    if entity.scale.x <= 0 then
+    if entity.scale.x <= -100 then
         currSpeed = scaleSpeed
     end
     
     if entity.scale.y >= maxScale then
         currSpeed = -scaleSpeed
     end
-    if entity.scale.y <= 0 then
+    if entity.scale.y <= -100 then
         currSpeed = scaleSpeed
     end
 
